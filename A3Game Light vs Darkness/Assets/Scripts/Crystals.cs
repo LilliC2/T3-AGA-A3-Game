c08d0c1647d821d7fbd14b7795fcb1585db0566b
LilliC2 <lillichuawiwat@gmail.com>
2022-12-05 12:34:33 +1100

    Wk 11 In Class Work
    Added:
    - Begun adding player UI
    - Fixed hitting bugs glitch
    - added crystal assests
    - added crystal glow

---
+++ A3Game Light vs Darkness/Assets/Scripts/Crystals.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystals : GameBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision);
+
+        if(collision.gameObject.CompareTag("Sword"))
+        {
+            //only take damage if player is attacking
+            if (_P.playerState == ThirdPersonMovement.PlayerState.Attack)
+            {
+                StartCoroutine( GlowStartUp());
+            }
+        }    
+    }
+
+    IEnumerator GlowStartUp()
+    {
+        AnimationTrigger("LightUp");
+        yield return new WaitForSeconds(1);
+        anim.SetBool("Glow", true);
+    }
+}
